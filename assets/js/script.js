40a82ce211cb280190545498bd88fdab58420c34
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-06-05 20:28:10 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,20 @@
 const faqItem = document.querySelectorAll('.faq__item');
-const resposta = document.querySelector('.faq__item p');
-const titulo = document.querySelectorAll('#faq__item h3');
-const pagina = document.documentElement;
-
-
-titulo.forEach((item) => {
-    item.addEventListener('click', ativarTexto);
-    function ativarTexto(el){
-            if(el.target == item){
-                document.body.classList.toggle('ativo');
-                resposta.setAttribute('id', 'resposta');
-            } 
-          
-        }
-    });
+const texto = document.getElementsByTagName('p');
+const fora = document.querySelector('.faq__container');
 
 
 
+faqItem.forEach((divs) =>{
+    divs.addEventListener('click', ativarTexto);
+
+});
 
 
+function ativarTexto(event){
+    faqItem.forEach((item) =>{
+        item.classList.remove('ativar');
+    })
 
-
-
+    event.currentTarget.classList.add('ativar');
+    
+}
