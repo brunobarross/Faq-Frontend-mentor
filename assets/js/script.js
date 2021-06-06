4c9a02fdf4743ef27e76fd3e5e629f2f6bbc7ca5
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-06-06 20:24:27 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,42 @@
 const faqItem = document.querySelectorAll('.faq__item');
 const texto = document.getElementsByTagName('p');
-const fora = document.querySelector('.faq');
+const fundo = document.querySelector('.bg');
+const fundoBranco = document.querySelector('.faq');
+
+// remover resposta da faq
+
+fundo.addEventListener('click', removerTexto);
+function removerTexto(event){
+    if(event.target == fundo){
+        faqItem.forEach((item) =>{
+            item.classList.remove("ativar");
+        })
+    
+    } 
+}
 
 
-
-
-
-
-
+// adicionar rspostas ao clicar na pergunta
 
 faqItem.forEach((divs) =>{
     divs.addEventListener('click',  ativarTexto);
-
-    
-
-
 });
 
-
-/*
-
-
-}
-
-*/
-
 function ativarTexto(event){
-
     faqItem.forEach((item) =>{
             item.classList.remove('ativar');
-
-    })
-
+    });
     event.currentTarget.classList.toggle('ativar');
+
+// sumir perguntas após 1 minuto 
+    setTimeout(function tempoTexto(){
+        faqItem.forEach((item) =>{
+            if(item.classList.contains('ativar')){
+                item.classList.remove("ativar");
+            }
+            
+        })
+    }, 60000);
 }
+
 
